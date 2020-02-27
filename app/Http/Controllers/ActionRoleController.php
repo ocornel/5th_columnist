74a4c3a50ce864cf7681aea25fa0c74258915ea0
Martin Cornel <mrtncornel@gmail.com>
2020-02-27 17:08:31 +0300

    Migrations, models and controllers.

---
+++ app/Http/Controllers/ActionRoleController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\ActionRole;
+use Illuminate\Http\Request;
+
+class ActionRoleController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\ActionRole  $actionRole
+     * @return \Illuminate\Http\Response
+     */
+    public function show(ActionRole $actionRole)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\ActionRole  $actionRole
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(ActionRole $actionRole)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\ActionRole  $actionRole
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, ActionRole $actionRole)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\ActionRole  $actionRole
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(ActionRole $actionRole)
+    {
+        //
+    }
+}
